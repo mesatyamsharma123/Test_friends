7a29e3e068072b1de84a5dc5a762e8922d004683
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2024-06-09 00:08:43 +0530

    kk

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './component/Home'
 import Slider from './component/slider/Slider'
 import Detail from './component/detail/Detail'
@@ -8,32 +8,47 @@ import Detail2 from './component/detail/Detail2'
 import Detail3 from './component/detail/Detail3'
 import Detail4 from './component/detail/Detail4'
 const App = () => {
-    const route=createBrowserRouter([
-        { path: '/',
-            element:<Home/>
-        },
-        { path: '/slider',
-          element:<Slider/>
-      },
-      { path: '/detail',
-        element:<Detail/>
-    },
-    { path: '/detail1',
-      element:<Detail1/>
-  }, { path: '/detail2',
-    element:<Detail2/>
-}, { path: '/detail3',
-  element:<Detail3/>
-}, { path: '/detail4',
-  element:<Detail4/>
-},
 
 
-    ])
+
+//     const route=createBrowserRouter ([
+//         { path: '/',
+//             element:<Home/>
+//         },
+//         { path: '/slider',
+//           element:<Slider/>
+//       },
+//       { path: '/detail',
+//         element:<Detail/>
+//     },
+//     { path: '/detail1',
+//       element:<Detail1/>
+//   }, { path: '/detail2',
+//     element:<Detail2/>
+// }, { path: '/detail3',
+//   element:<Detail3/>
+// }, { path: '/detail4',
+//   element:<Detail4/>
+// },
+
+
+  //  ])
   return (
     <div className="App">
 
- <RouterProvider router={route}></RouterProvider>
+<BrowserRouter>
+<Routes>
+  <Route exact path="/" element={<Home/>}></Route>
+  <Route exact path="/slider" element={<Slider/>}></Route>
+  <Route exact path="/detail" element={<Detail/>}></Route>
+  <Route exact path="/deatil1" element={<Detail1/>}></Route>
+  <Route exact path="/detail2" element={<Detail2/>}></Route>
+  <Route exact path="/detail3" element={<Detail3/>}></Route>
+  <Route exact path="/detail4" element={<Detail4/>}></Route>
+</Routes>
+
+</BrowserRouter>
+
 
     </div>
   )
